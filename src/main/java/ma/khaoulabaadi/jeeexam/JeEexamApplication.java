a6dc2344165f9e7e19d25a85151956f8de1e50de
baadikhaoula <baadikhaoula38@gmail.com>
2026-05-11 11:00:33 +0200

    Initial Spring Boot Project

---
+++ src/main/java/ma/khaoulabaadi/jeeexam/JeEexamApplication.java
@@ -0,0 +1,13 @@
+package ma.khaoulabaadi.jeeexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeEexamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JeEexamApplication.class, args);
+    }
+
+}
